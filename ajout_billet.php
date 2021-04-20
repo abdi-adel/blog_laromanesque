ed99f69475969643c235c55c03bb0d2d75f4a9cf
ABDI ADEL <66871090+abdi-adel@users.noreply.github.com>
2021-04-20 19:20:16 +0200

    trying to connect to the herolu db

--- ajout_billet.php
+++ ajout_billet.php
@@ -1,7 +1,20 @@
 <?php
+
+//Get Heroku ClearDB connection information
+$cleardb_url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+$cleardb_server = $cleardb_url["host"];
+$cleardb_username = $cleardb_url["user"];
+$cleardb_password = $cleardb_url["pass"];
+$cleardb_db = substr($cleardb_url["path"],1);
+$active_group = 'default';
+$query_builder = TRUE;
+
+// Connect to DB
+
 try
 {
-	$bdd = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    $conn = mysqli_connect($cleardb_server, $cleardb_username, $cleardb_password, $cleardb_db);
+	// $bdd = new PDO('mysql:host=localhost;dbname=blog;charset=utf8', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 }
 catch (Exception $e)
 {
@@ -9,7 +22,8 @@ catch (Exception $e)
 }
 
 
-$req = $bdd -> prepare('INSERT INTO billets (auteur, titre, contenu) VALUES(?, ?, ?)') or die(print_r($bdd->errorInfo()));
+// $req = $bdd -> prepare('INSERT INTO billets (auteur, titre, contenu) VALUES(?, ?, ?)') or die(print_r($bdd->errorInfo()));
+$req = $conn -> prepare('INSERT INTO billets (auteur, titre, contenu) VALUES(?, ?, ?)') or die(print_r($conn->errorInfo()));
 $req -> execute (array(
     nl2br(strip_tags($_POST["auteur_billet"])),
     nl2br(strip_tags($_POST["titre_billet"])),
